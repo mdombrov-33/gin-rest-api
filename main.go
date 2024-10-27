74c245345fe8582a7b937e73e9b4583fa60955bb
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-27 21:42:33 +0200

    add handle for events get request

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-// "github.com/gin-gonic/gin"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	// Create a new Gin instance with build-in default middleware and automatic recover from panics
+	server := gin.Default()
 
+	server.GET("/events", getEvents) // GET, POST, PUT, PATCH, DELETE
+
+	server.Run(":8080") // localhost:8080
+}
+
+func getEvents(context *gin.Context) {
+	//send back data in JSON format
+	context.JSON(http.StatusOK, gin.H{"message": "Hello!"}) // 200 status code, gin.H is a map we can use to send back data
 }
