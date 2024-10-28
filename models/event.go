ce95b2c126e78f8a09a02842f77f7e26e7c70559
Maksym Dombrov <maksymdombrov@gmail.com>
2024-10-28 09:47:13 +0200

    add store event in db functionality

--- models/event.go
+++ models/event.go
@@ -1,10 +1,14 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/mdombrov-33/ginrestapi/db"
+)
 
 type Event struct {
 	//`binding:"required"` is a struct tag that tells Gin to check if the field is present in the JSON body
-	ID          int
+	ID          int64
 	Title       string    `binding:"required"`
 	Description string    `binding:"required"`
 	Location    string    `binding:"required"`
@@ -14,9 +18,32 @@ type Event struct {
 
 var events = []Event{}
 
-func (e Event) Save() {
-	//later: Save the event to the database
-	events = append(events, e)
+func (e Event) Save() error {
+	query := `INSERT INTO events (title, description, location, dateTime, user_id)
+	VALUES (?, ?, ?, ?, ?)` // protect against SQL injection, actual insert happens below
+
+	// Prepare the query to prevent SQL injection attacks and to improve performance
+	sqlStatement, err := db.DB.Prepare(query)
+	if err != nil {
+		return err
+	}
+
+	// Close the statement when the function ends
+	defer sqlStatement.Close()
+
+	// Insert values in the same order as the query above
+	result, err := sqlStatement.Exec(e.Title, e.Description, e.Location, e.DateTime, e.UserID)
+	if err != nil {
+		return err
+	}
+
+	// Get the ID of the inserted row, we can do that because we set the ID to AUTOINCREMENT
+	id, err := result.LastInsertId()
+
+	e.ID = id
+
+	return err
+
 }
 
 func GetAllEvents() []Event {
